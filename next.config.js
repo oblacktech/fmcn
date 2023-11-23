29527cf582094a10c90852a06c5b8f4db848ff58
vincent <uveghobamien@gmail.com>
2023-11-23 16:03:40 +0100

    setup

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< HEAD
-const nextConfig = {
-
-  reactStrictMode:true
-}
-=======
 const nextConfig = {}
->>>>>>> cb2de52f7da2c7f6299f44d9c84af1fbd1b714e7
 
 module.exports = nextConfig
